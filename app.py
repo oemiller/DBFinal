57edb2bfd2b467fce097e43e779d764796d4884e
Owen Miller <oemiller@mines.edu>
2025-04-23 15:04:37 -0600

    added the page for all data

--- app.py
+++ app.py
@@ -3,14 +3,29 @@ import pg8000
 import os
 
 app = Flask(__name__)
-database_connection = None
+
+def get_column_names(cursor):
+    column_names = []
+    for column in cursor.description:
+        column_names.append(column[0])
+    return column_names
+
 
 @app.route("/")
 def index():
     return render_template("index.html", param="")
 
+@app.route("/full_table")
+def full_table():
+    cursor = database_connection.cursor()
+    cursor.execute("SELECT * FROM shared_production_and_consumption_by_source") 
+    all = cursor.fetchall()
+    column_names = get_column_names(cursor)
+    cursor.close()
+    return render_template("full_table.html", column_names=column_names, items=all)
+
 @app.route("/test")
-def test():
+def test_page():
     return render_template("index.html", param="")
 
 if __name__ == "__main__":
@@ -34,9 +49,8 @@ if __name__ == "__main__":
         host=host,
         database=database
     )
+    print(type(database_connection))
     cursor = database_connection.cursor()
     cursor.execute("SET search_path TO group39") 
-    cursor.execute("SELECT * FROM test_table") 
-    result = cursor.fetchall() 
-    print("query result", result)
+    cursor.close()
     app.run(debug=False)
